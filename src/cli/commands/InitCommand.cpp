bc667ab595c42a99aef95060b5139a9a46595e31
Sahil Gupta <sahilgupta.17299@gmail.com>
2025-11-02 16:48:16 +0530

    fix init command msg for showing relative path

--- src/cli/commands/InitCommand.cpp
+++ src/cli/commands/InitCommand.cpp
@@ -12,20 +12,24 @@ Expected<void> InitCommand::execute(const AppContext&, const std::vector<std::st
     if (!args.empty()) {
         target = args.front();
     }
+    
+    // Resolve absolute path for init
+    std::filesystem::path absoluteTarget = std::filesystem::absolute(target);
+    
     auto res = Repository::instance().init(target);
     if (!res) {
         if (res.error().code == ErrorCode::AlreadyInitialized) {
-            std::cout << "Gitter repository is already initialised in "
-                << std::filesystem::absolute(target).string() << "/.gitter/\n";
+            std::cout << "Gitter repository is already initialised in ./"
+                << absoluteTarget.filename().string() << "/.gitter/\n";
         }
         else {
-            std::cout << "Failed to initialize Gitter repository in "
-                << std::filesystem::absolute(target).string() << "/.gitter/\n";
+            std::cout << "Failed to initialize Gitter repository in ./"
+                << absoluteTarget.filename().string() << "/.gitter/\n";
 
         }
     } else {
-        std::cout << "Initialized empty Gitter repository in "
-            << std::filesystem::absolute(target).string() << "/.gitter/\n";
+        std::cout << "Initialized empty Gitter repository in ./"
+            << absoluteTarget.filename().string() << "/.gitter/\n";
     }
     return {};
 }
